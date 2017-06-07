950a2814a0e8688fe809c3f74523736d78f19966
buiquangthang <buiquangthangit@gmail.com>
2017-06-07 10:34:11 +0700

    add validate client side login form

--- app/assets/javascripts/education/users.js
+++ app/assets/javascripts/education/users.js
@@ -19,3 +19,35 @@ $(document).ready(function(){
     })
   })
 })
+
+function validateFormLogin() {
+  var inputEmail = $('#user_email');
+  var inputPassword = $('#user_password');
+  var textEmail = inputEmail.val();
+  var textPassword = inputPassword.val();
+  var flagValidate = true;
+  $('.col_full').removeClass('has-error');
+  $('span').remove('.help-block');
+  if(textEmail.length > 255) {
+      flagValidate = false;
+      addErrorMessage(inputEmail, I18n.t("devise.sessions.form_login.too_long_255"))
+  }
+  if(textPassword.length > 255) {
+    flagValidate = false;
+    addErrorMessage(inputPassword, I18n.t("devise.sessions.form_login.too_long_255"))
+  }
+  if(textPassword.length < 6) {
+    flagValidate = false;
+    addErrorMessage(inputPassword, I18n.t("devise.sessions.form_login.too_short_6"));
+  }
+  if(flagValidate){
+    $('#sign-in-user').submit();
+  }
+  return false;
+}
+
+function addErrorMessage(inputField, errorText) {
+  var $divFormGroup = inputField.parent();
+  $divFormGroup.addClass('has-error');
+  $divFormGroup.append('<span class="help-block">' + errorText + '</span>');
+}
